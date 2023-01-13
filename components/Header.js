0003c4f525356d832dea49139749d495ad131dc7
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-13 08:19:52 +0100

    Icons

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import { HomeIcon, MagnifyingGlassIcon, UserIcon } from "@heroicons/react/24/outline";
+import { CheckBadgeIcon, BoltIcon, RectangleStackIcon, HomeIcon, MagnifyingGlassIcon, UserIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import HeaderItem from "./HeaderItem";
 
@@ -7,6 +7,11 @@ const Header = () => {
         <header className="">
             <div className="">
                 <HeaderItem title='HOME ' Icon={HomeIcon} />
+                <HeaderItem title='TRENDING ' Icon={BoltIcon} />
+                <HeaderItem title='VERIFIED ' Icon={CheckBadgeIcon} />
+                <HeaderItem title='COLLECTIONS ' Icon={RectangleStackIcon} />
+                <HeaderItem title=' SEARCH' Icon={MagnifyingGlassIcon} />
+                <HeaderItem title=' ACCOUNT' Icon={UserIcon} />
             </div>
             <Image className="object-contain" src="https://links.papareact.com/ua6" width={200} height={100} alt="img" />
         </header>
--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,8 +1,8 @@
 const HeaderItem = ({ Icon, title }) => {
     return (
-        <div>
-            <Icon className="h-8" />
-            <p>{title}</p>
+        <div className="flex flex-col items-center cursor-pointer group w12 sm:w-20 hover:text-white">
+            <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+            <p className="opacity-0 group-hover:opacity-100 tracking-widest">{title}</p>
         </div>
     );
 };
