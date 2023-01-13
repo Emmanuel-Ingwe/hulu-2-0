89b05892ae850088dc81a80f0b6199e15fcc5ace
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-13 05:45:27 +0100

    Header

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="">
+            <h1>This Is Header</h1>
+        </header>
+    );
+};
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
 import Head from 'next/head';
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Hulu 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>HELLO</h1>
+      {/* HEADER */}
+      <Header />
+
+      {/* NAV */}
+
+      {/* RESULTS */}
     </div>
   );
 }
