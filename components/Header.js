fd52105e7fda38c60277a3a9eed8133f7f21121e
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-13 06:21:55 +0100

    Setup comp.

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,12 @@
+import Image from "next/image";
+
 const Header = () => {
     return (
         <header className="">
             <h1>This Is Header</h1>
+            <Image className="object-contain" src="https://links.papareact.com/ua6" width={200} height={100} alt="img" />
         </header>
     );
 };
 
-export default Header
+export default Header;
--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["links.papareact.com", "images.tmdb.org"],
+  },
+};
+
