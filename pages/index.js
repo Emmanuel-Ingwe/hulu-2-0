4d17cc38e5c584cf1404e2a38bb29fbfbbd25570
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-13 17:03:12 +0100

    requests

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Header from "../components/Header";
 import Nav from '../components/Nav';
 import Results from '../components/Results';
+import requests from "../utilis/requests";
 
 export default function Home() {
   return (
@@ -25,4 +26,14 @@ export default function Home() {
 
 export async function getServerSideProps(context) {
   const genre = context.query.genre;
+  const request = await fetch(
+    `https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
