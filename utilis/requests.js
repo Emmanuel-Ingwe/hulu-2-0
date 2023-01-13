3cc33ca7f767e6e04003a1b87573fc3f4f532f87
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-13 12:17:20 +0100

    requests

--- utilis/requests.js
+++ utilis/requests.js
@@ -1,48 +1,50 @@
 const API_KEY = process.env.API_KEY;
 
-export default {
+const requests = {
     fetchTrending: {
-        title: 'Trending',
-        url: '/trending/all/week?api_key=${API_KEY}&language=en-US',
+        title: "Trending",
+        url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
     },
     fetchTopRated: {
-        title: 'Top Rated',
-        url: '/movie/top_rated?api_key=${API_KEY}&language=en-US',
+        title: "Top Rated",
+        url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`
     },
-    fetch: {
-        title: 'Trending',
-        url: '/discover/movie?api_key=${API_KEY}&with_genres=28',
+    fetchActionMovies: {
+        title: "Action",
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=28`
     },
-    fetch: {
-        title: 'Trending',
-        url: '/discover/movie?api_key=${API_KEY}&with_genres=35',
+    fetchComedyMovies: {
+        title: "Comedy",
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=35`
     },
-    fetch: {
-        title: 'Trending',
-        url: '/discover/movie?api_key=${API_KEY}&with_genres=27',
+    fetchHorroeMovies: {
+        title: "Horror",
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=27`
     },
-    fetch: {
-        title: 'Trending',
-        url: '/discover/movie?api_key=${API_KEY}&with_genres=10749',
+    fetchRomanceMovies: {
+        title: "Romance",
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`
     },
-    fetch: {
-        title: 'Trending',
-        url: '/discover/movie?api_key=${API_KEY}&with_genres=9648',
+    fetchMystery: {
+        title: "Mystery",
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`
     },
-    fetch: {
-        title: 'Trending',
-        url: '/discover/movie?api_key=${API_KEY}&with_genres=878',
+    fetchSciFi: {
+        title: "Sci-Fi",
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=878`
     },
-    fetch: {
-        title: 'Trending',
-        url: '/discover/movie?api_key=${API_KEY}&with_genres=37',
+    fetchWestern: {
+        title: "Western",
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=37`
     },
-    fetch: {
-        title: 'Animation',
-        url: '/discover/movie?api_key=${API_KEY}&with_genres=16',
+    fetchAnimation: {
+        title: "Animation",
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=16`
     },
-    fetch: {
-        title: 'TV Movie',
-        url: '/discover/movie?api_key=${API_KEY}&with_genres=10770',
+    fetchTv: {
+        title: "TV Movie",
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`
     },
 };
+
+export default requests;
