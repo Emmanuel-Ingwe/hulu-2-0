81009820ab45fdb883678b6ab2aad4021f8e9a64
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-13 15:51:31 +0100

    Results.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Header from "../components/Header";
 import Nav from '../components/Nav';
+import Results from '../components/Results';
 
 export default function Home() {
   return (
@@ -17,6 +18,11 @@ export default function Home() {
       <Nav />
 
       {/* RESULTS */}
+      <Results />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+
 }
