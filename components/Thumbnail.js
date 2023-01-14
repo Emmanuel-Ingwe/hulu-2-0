5ce7d200a6b502c9a8ad4a5759537b1bf91810d0
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-14 09:14:35 +0100

    own styles

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -4,6 +4,7 @@ import { forwardRef } from "react";
 
 const Thumbnail = forwardRef(({ result }, ref) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
+
     return (
         <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
             <Image
