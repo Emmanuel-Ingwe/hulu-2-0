3cbf5163dbb6937143f60492e685852bbc9a4de1
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-14 22:52:26 +0100

    nextConfg

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   reactStrictMode: true,
   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/account123/**',
+      },
+    ],
     domains: ["links.papareact.com", "images.tmdb.org"],
   },
 };
